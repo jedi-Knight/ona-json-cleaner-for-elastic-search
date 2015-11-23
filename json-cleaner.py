1c8609ddb31267965a4e547a0ae0cf8ad19e6083
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-23 07:32:12 -0500

    jsoncleaner.py

--- json-cleaner.py
+++ json-cleaner.py
@@ -5,30 +5,40 @@ import re
 
 itemArray = open('hda.json', 'r')
 
+keyshorteneddatadumpFile = open('hda.keyshortenened.json','w')
+keymappingFile = open('hda.keydef.json', 'w')
+
 itemArray = itemArray.read()
 
 itemArray = json.loads(itemArray)
 
 keyDictionary = {}
 
+keyShortenedDatadump = []
+
 clashAvoidedCount = 0
 
 for index, item in enumerate(itemArray):
 	tempKeyDictionary = {}
+	itemHolder = {}
 	for k,v in item.items():
 		subK = re.sub(r'^_', r'', k)
 		subK = re.sub(r'(^.).*(.)/(\w+$)', r'\1_\2_\3', subK)
 		if subK not in tempKeyDictionary:
 			tempKeyDictionary[subK] = k
+			itemHolder[subK] = v
 		else:
 			clashAvoidedCount += 1
 			tempKeyDictionary[subK+str(clashAvoidedCount)] = k
+			itemHolder[subK+str(clashAvoidedCount)] = v
 	keyDictionary.update(tempKeyDictionary)
-print(json.dumps(keyDictionary))
+	keyShortenedDatadump.append(itemHolder)	
 
+keymappingFile.write(json.dumps(keyDictionary))
 
+keyshorteneddatadumpFile.write(json.dumps(keyShortenedDatadump))
 
-
+print(clashAvoidedCount)
 
 
 
