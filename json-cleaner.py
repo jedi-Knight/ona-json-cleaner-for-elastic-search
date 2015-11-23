0d59ac0d5d1f7acd77a2ba711373462df1c9bfc5
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-23 06:45:19 -0500

    json-def-mapping

--- json-cleaner.py
+++ json-cleaner.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 
+import json
+import re
 
-itemArray = fopen('hda.json', 'r')
+itemArray = open('hda.json', 'r')
 
 itemArray = itemArray.read()
 
-print itemArray
+itemArray = json.loads(itemArray)
 
+keyDictionary = {}
 
+clashAvoidedCount = 0
 
+for index, item in enumerate(itemArray):
+	tempKeyDictionary = {}
+	for k,v in item.items():
+		subK = re.sub(r'^_', r'', k)
+		subK = re.sub(r'(^.).*(.)/(\w+$)', r'\1_\2_\3', subK)
+		if subK not in tempKeyDictionary:
+			tempKeyDictionary[subK] = k
+		else:
+			clashAvoidedCount += 1
+			tempKeyDictionary[subK+str(clashAvoidedCount)] = k
+	keyDictionary.update(tempKeyDictionary)
+print(keyDictionary)
 
 
 
