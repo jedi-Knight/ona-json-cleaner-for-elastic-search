0bafd057b0d0b01df36b0807c09417b4370a5ae7
Pratik Gautam <gautamxpratik@gmail.com>
2015-12-01 02:27:25 -0500

    datagen.py

--- datagen.py
+++ datagen.py
@@ -3,11 +3,26 @@
 import json
 import re
 from copy import deepcopy
-import os
+import hashlib
+import sys
 
-itemArray = open('hda.json', 'r')
+itemArray = open(sys.argv[1], 'r')
 
-gendatafile = open('data.json','w')
+gendatafile = open('data.gen.'+sys.argv[1], 'w')
+
+start = 0
+size = 1000
+
+try:
+	start = int(sys.argv[2])
+	size = start+1000
+except:
+	print('ok')
+
+try:
+	size = int(sys.argv[3])
+except:
+	print('ok again')
 
 itemArray = itemArray.read()
 
@@ -21,11 +36,13 @@ for index, item in enumerate(itemArray):
 	for k,v in item.items():
 		defSet[k] = v
 
-for c in range(0,100):
-	defSet['_id'] = c
-	del defSet['_uuid']
-	gendata.append(defSet)
-	defSet['_uuid'] = os.popen("md5sum <<'"+json.dumps(defSet)+"'").read().replace('\n', '')
+for c in range(start, start+size):
+	dataSet = deepcopy(defSet)
+	dataSet['_id'] = c
+	del dataSet['_uuid']
+	#dataSet['_uuid'] = os.popen("md5sum <<'"+str(c)+"'").read().replace('\n', '')
+	dataSet['_uuid'] = hashlib.md5(json.dumps(dataSet).encode('utf-8')).hexdigest()
+	gendata.append(dataSet)
 
 gendatafile.write(json.dumps(gendata))
 
